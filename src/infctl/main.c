74c1941e486f7d389b19187fb2cb9ba1c7741dc3
James Magahern <james@magahern.com>
2019-10-05 17:55:37 -0700

    Clean up infctl commands

--- src/infctl/main.c
+++ src/infctl/main.c
@@ -9,30 +9,33 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+
+static void test_dynamic_pixmap (infdevice_t *device, char **args);
+static void test_pixmap_bmp (infdevice_t *device, char **argv);
+
+typedef struct {
+    const char *name;
+    void (*function)(infdevice_t*, char**);
+} command_t;
+
+static command_t __commands[] = {
+    { "pixmap", test_dynamic_pixmap },
+    { "bmp",    test_pixmap_bmp },
+};
 
 static void print_usage (const char *progname)
 {
-    fprintf(stderr, "Usage: %s [key_id] [bmp_file]\n", progname);
-    fprintf(stderr, "BMP file must be 72x72, 24-bits (R8 G8 B8), no colorspace info\n");
+    fprintf (stderr, "Usage: %s [command] [arguments...]\n", progname);
+    fprintf (stderr, "Commands: \n");
+    fprintf (stderr, "\tbmp [key_id] [bmp_file]: Load BMP file\n");
+    fprintf (stderr, "\t\tBMP file must be 72x72, 24-bits (R8 G8 B8), no colorspace info\n");
+    fprintf (stderr, "\tpixmap: Test dynamically generated pixmaps\n");
 }
 
-int main (int argc, char **argv)
+static void test_dynamic_pixmap (infdevice_t *device, char **args)
 {
-    if (argc < 3) {
-        print_usage (argv[0]);
-        return 1;
-    }
-
-    const int key_id = strtod (argv[1], NULL);
-    const char *bmp_path = argv[2];
-
-    infdevice_t *device = infdevice_open ();
-    if (!device) {
-        fprintf(stderr, "Could not open device\n");
-        return 1;
-    }
-
     cairo_surface_t *surface = infpixmap_create_surface ();
     cairo_t *cr = cairo_create (surface);
 
@@ -58,19 +61,50 @@ int main (int argc, char **argv)
 
         usleep(5000);
     }
+}
 
-    /*
+static void test_pixmap_bmp (infdevice_t *device, char **argv)
+{
+    const int key_id = strtod (argv[1], NULL);
+    const char *bmp_path = argv[2];
 
     infpixmap_t *pixmap = infpixmap_open_file (bmp_path);
     if (!pixmap) {
         fprintf(stderr, "Unable to open BMP file\n");
-        return 1;
+        return;
     }
 
     infdevice_set_pixmap_for_key_id (device, key_id, pixmap);
-
     infpixmap_free (pixmap);
-    */
+}
+
+int main (int argc, char **argv)
+{
+    if (argc < 2) {
+        print_usage (argv[0]);
+        return 1;
+    }
+
+    infdevice_t *device = infdevice_open ();
+    if (!device) {
+        fprintf(stderr, "Could not open device\n");
+        return 1;
+    }
+
+    unsigned int command_idx = 0;
+    size_t num_commands = sizeof (__commands) / sizeof (command_t);
+    for (; command_idx < num_commands; command_idx++) {
+        const command_t command = __commands[command_idx];
+        if (strncmp (argv[1], command.name, strlen(command.name)) == 0) {
+            command.function (device, argv);
+            break;
+        }
+    }
+
+    if (command_idx == num_commands) {
+        fprintf (stderr, "Command not found\n");
+        print_usage (argv[0]);
+    }
 
     infdevice_close (device);
 
