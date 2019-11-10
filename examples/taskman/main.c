f256fd2b01f4f294ebdf7c5bb7394aa99b60d929
James Magahern <james@magahern.com>
2019-11-10 15:23:57 -0800

    taskman: getting window properties

--- examples/taskman/main.c
+++ examples/taskman/main.c
@@ -1,26 +1,32 @@
 #include <infinitton/infinitton.h>
+
+#include <assert.h>
 #include <X11/Xlib.h>
 #include <X11/Xos.h>
 #include <stdio.h>
 #include <sys/param.h> // max, min
 
 #define MAX_STR 500000
-#define WINDOW(win) (long unsigned int *)win
+#define WINDOW(win) (long unsigned int)win
 
 Display *__display = NULL;
 Window *__root_window = NULL;
 
 static const char *
-get_window_property_and_type (Atom  atom, 
+get_window_property_and_type (Window *window,
+                              Atom  atom, 
                               long *length, 
                               Atom *type, 
                               int  *size)
 {
-    static unsigned char *prop = NULL;
+    unsigned char *prop = NULL;
+
+#if 0 // callers must free!
     if (prop) {
         XFree (prop);
         prop = NULL;
     }
+#endif
 
     Atom actual_type;
     int actual_format;
@@ -28,7 +34,7 @@ get_window_property_and_type (Atom  atom,
     unsigned long bytes_after;
     int status = XGetWindowProperty (
             __display, 
-            WINDOW(__root_window), 
+            WINDOW(window), 
             atom,
             0,
             (MAX_STR + 3) / 4,
@@ -69,7 +75,7 @@ get_window_property_and_type (Atom  atom,
 
     *length = MIN (nitems * nbytes, MAX_STR);
     *type = actual_type;
-    *size = actual_format;
+    *size = nbytes; // actual_format;
     return (const char *)prop;
 }
 
@@ -87,7 +93,44 @@ int main (int argc, char **argv)
         return 1;
     }
 
+    Atom atom = XInternAtom (__display, "_NET_CLIENT_LIST", True);
+    if (atom == BadAtom) {
+        fprintf (stderr, "Bad atom.\n");
+        return 1;
+    }
+
+    Atom type;
+    int item_size, length;
     const char *result = get_window_property_and_type (
+        __root_window,
+        atom,
+        &length,
+        &type,
+        &item_size
+    );
+
+    // Items are Window pointers
+    unsigned nitems = (length / item_size);
+    printf ("num windows: %d\n", nitems);
+
+    Atom name_atom = XInternAtom (__display, "WM_NAME", True);
+    Atom icon_atom = XInternAtom (__display, "_NET_WM_ICON", True);
+
+    unsigned long int *windows = (unsigned long int *)result;
+    for (unsigned i = 0; i < nitems; i++) {
+        printf ("window id: 0x%x\n", windows[i]);
+        
+        int proplen, propsize;
+        result = get_window_property_and_type (
+                windows[i],
+                name_atom,
+                &proplen,
+                &type,
+                &propsize
+        );
+
+        printf("    name: %s\n", result);
+    }
 
     return 0;
 }
